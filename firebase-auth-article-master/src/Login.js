f32eb8d7467f8806a6b866affe6511959073971a
Richeton <e0727093@u.nus.edu>
2024-10-27 22:36:20 +0800

    Modified Login.js

--- firebase-auth-article-master/src/Login.js
+++ firebase-auth-article-master/src/Login.js
@@ -48,7 +48,7 @@ function Login() {
           <Link to="/reset">Forgot Password</Link>
         </div>
         <div>
-          Don't have an account? <Link to="/register">Register here</Link>  now.
+          Don't have an account? <Link to="/register">Register here</Link>  right now.
         </div>
       </div>
     </div>
